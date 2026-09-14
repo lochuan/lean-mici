7d152137bcbbb5a3ffdff56f08c4d821397648fe
lochuan <lochuan@users.noreply.github.com>
2026-09-14 11:51:46 +0800

    params: LanLinkEnabled persistent (lanlink is the primary device UI)

--- openpilot/common/params_keys.h
+++ openpilot/common/params_keys.h
@@ -86,7 +86,7 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"LocationFilterInitialState", {PERSISTENT, BYTES}},
     {"LateralManeuverMode", {CLEAR_ON_MANAGER_START | CLEAR_ON_OFFROAD_TRANSITION, BOOL}},
     {"LanLinkPasswordHash", {PERSISTENT | DONT_LOG, STRING}},
-    {"LanLinkEnabled", {CLEAR_ON_MANAGER_START, BOOL, "0"}},
+    {"LanLinkEnabled", {PERSISTENT, BOOL, "1"}},
     {"LanLinkParamsVersion", {PERSISTENT, INT, "0"}},
     {"LongitudinalManeuverMode", {CLEAR_ON_MANAGER_START | CLEAR_ON_OFFROAD_TRANSITION, BOOL}},
     {"LongitudinalPersonality", {PERSISTENT | BACKUP, INT, std::to_string(static_cast<int>(cereal::LongitudinalPersonality::STANDARD))}},
